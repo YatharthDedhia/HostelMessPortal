fde1abaa31b4d6f111a7039bd01737c3cac19911
Manav <manavshah.2003.ms@gmail.com>
2023-04-22 12:41:16 +0530

    Leaves Request Added

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -6,7 +6,6 @@ const User = require('../models/User');
 
 const depositMoney=catchAsyncErrors(async(req,res,next)=>{
     const user=await User.findOne({"email":req.body.email})
-    console.log(user._id);
     const account1=await Account.findOne({"user":user._id});
     const deposit = { amount: req.body.amount };
     const account = await Account.findOneAndUpdate(
@@ -15,9 +14,9 @@ const depositMoney=catchAsyncErrors(async(req,res,next)=>{
           $inc: { amount: req.body.amount },
           $push: { deposits: deposit },
         },
-        // { new: true ,
-        //     select: { _id: 0, deposits: { _id: 0 } } // exclude _id and deposits._id
-        // }
+        { new: true ,
+            // select: { _id: 0, deposits: { _id: 0 } } // exclude _id and deposits._id
+        }
         
       );
     console.log(account);
@@ -29,7 +28,39 @@ const depositMoney=catchAsyncErrors(async(req,res,next)=>{
     
     
 });
+const applyLeaves = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ "email": req.body.email });
+
+  console.log(user._id);
+
+  const startDate = new Date(req.body.startDate);
+  const endDate = new Date(req.body.endDate);
+  const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
+  const leaveDays = Math.round((endDate.getTime() - startDate.getTime()) / oneDay) + 1;
+
+  const leave = { startDate: req.body.startDate ,endDate:req.body.endDate,mealType:req.body.mealType };
+  const account = await Account.findOneAndUpdate(
+    { "user": user._id },
+    {
+      $push: { leaves: leave },
+    },
+    { new: true ,
+        select: { _id: 0, deposits: { _id: 0 } } // exclude _id and deposits._id
+    }
+    
+  );
+  res.status(200).json({
+    success:true,
+    account,
+})
+
+
+
+  
+
+});
 module.exports = {
     // getAllAccounts,
-    depositMoney
+    depositMoney,
+    applyLeaves
 }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,6 @@ const accountController=require('../controllers/accountController');
 const router = express.Router();
 
 router.route('/deposit').post(accountController.depositMoney);
+router.route('/leaves').post(accountController.applyLeaves);
 
 module.exports=router;
